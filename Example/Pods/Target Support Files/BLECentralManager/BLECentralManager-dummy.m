928e681b0c30f4e86e84818aed97b87a1f528595
Balázs Kilvády <bkilvady@gmail.com>
2016-06-20 13:14:50 +0200

    Pod files.

---
+++ Example/Pods/Target Support Files/BLECentralManager/BLECentralManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BLECentralManager : NSObject
+@end
+@implementation PodsDummy_BLECentralManager
+@end
---
+++ Example/Pods/Target Support Files/BLECentralManager/BLECentralManager-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double BLECentralManagerVersionNumber;
+FOUNDATION_EXPORT const unsigned char BLECentralManagerVersionString[];
+
