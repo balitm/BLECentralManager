eee768c4426d3baba56099359ebd99bc4cb17a9b
Balázs Kilvády <bkilvady@gmail.com>
2016-06-28 17:23:59 +0200

    Rename iOS project part 2.

---
+++ Example/Pods/Target Support Files/Pods-BLECentralManager_iOS_Example/Pods-BLECentralManager_iOS_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BLECentralManager_iOS_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BLECentralManager_iOS_ExampleVersionString[];
+
